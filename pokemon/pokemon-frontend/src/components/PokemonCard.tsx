844dbabb0aa91fd71a480a50fe7dfe7896d6186d
zhouyi-c <zhouchi.stu@gmail.com>
2025-06-18 22:39:40 +0800

    feat: 添加搜索功能和详情弹窗
    - 新增宝可梦搜索功能，支持按名称、英文名或编号搜索
    - 添加点击卡片显示详情弹窗功能
    - 优化用户交互体验和响应式布局
    - 更新README文档说明新功能

--- pokemon/pokemon-frontend/src/components/PokemonCard.tsx
+++ pokemon/pokemon-frontend/src/components/PokemonCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card, Tag, Typography, Space } from 'antd';
+import React, { useState } from 'react';
+import { Card, Tag, Typography, Space, Modal, Descriptions, Image } from 'antd';
 import styled from 'styled-components';
 
 const { Title, Text } = Typography;
@@ -30,6 +30,14 @@ const TypeTag = styled(Tag)`
   font-size: 14px;
 `;
 
+const DetailImage = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: contain;
+  margin: 0 auto;
+  display: block;
+`;
+
 interface PokemonType {
   name: string;
   color: string;
@@ -52,53 +60,105 @@ interface PokemonCardProps {
 }
 
 const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon, onClick }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   const handleClick = () => {
+    setIsModalVisible(true);
     if (onClick) {
       onClick(pokemon);
     }
   };
 
+  const handleModalClose = () => {
+    setIsModalVisible(false);
+  };
+
   return (
-    <StyledCard
-      hoverable
-      onClick={handleClick}
-      cover={
-        <PokemonImage
-          src={pokemon.image_url}
-          alt={pokemon.name}
-          onError={(e) => {
-            const target = e.target as HTMLImageElement;
-            target.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png';
-          }}
+    <>
+      <StyledCard
+        hoverable
+        onClick={handleClick}
+        cover={
+          <PokemonImage
+            src={pokemon.image_url}
+            alt={pokemon.name}
+            onError={(e) => {
+              const target = e.target as HTMLImageElement;
+              target.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png';
+            }}
+          />
+        }
+      >
+        <Card.Meta
+          title={
+            <Space direction="vertical" size={0} style={{ width: '100%' }}>
+              <Title level={4} style={{ margin: 0 }}>
+                {pokemon.name}
+              </Title>
+              <Text type="secondary" style={{ fontSize: '14px' }}>
+                #{String(pokemon.pokemon_id).padStart(3, '0')} {pokemon.name_en}
+              </Text>
+            </Space>
+          }
+          description={
+            <Space wrap style={{ marginTop: 8 }}>
+              {pokemon.types.map(({ type }) => (
+                <TypeTag
+                  key={type.name}
+                  color={type.color}
+                  style={{ backgroundColor: type.color, color: 'white' }}
+                >
+                  {type.name}
+                </TypeTag>
+              ))}
+            </Space>
+          }
         />
-      }
-    >
-      <Card.Meta
+      </StyledCard>
+
+      <Modal
         title={
-          <Space direction="vertical" size={0} style={{ width: '100%' }}>
-            <Title level={4} style={{ margin: 0 }}>
-              {pokemon.name}
-            </Title>
-            <Text type="secondary" style={{ fontSize: '14px' }}>
-              #{String(pokemon.pokemon_id).padStart(3, '0')} {pokemon.name_en}
-            </Text>
+          <Space>
+            <span>{pokemon.name}</span>
+            <Text type="secondary">#{String(pokemon.pokemon_id).padStart(3, '0')}</Text>
           </Space>
         }
-        description={
-          <Space wrap style={{ marginTop: 8 }}>
-            {pokemon.types.map(({ type }) => (
-              <TypeTag
-                key={type.name}
-                color={type.color}
-                style={{ backgroundColor: type.color, color: 'white' }}
-              >
-                {type.name}
-              </TypeTag>
-            ))}
-          </Space>
-        }
-      />
-    </StyledCard>
+        open={isModalVisible}
+        onCancel={handleModalClose}
+        footer={null}
+        width={600}
+      >
+        <div style={{ textAlign: 'center', marginBottom: 24 }}>
+          <DetailImage
+            src={pokemon.image_url}
+            alt={pokemon.name}
+            onError={(e) => {
+              const target = e.target as HTMLImageElement;
+              target.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png';
+            }}
+          />
+        </div>
+        
+        <Descriptions column={1} bordered>
+          <Descriptions.Item label="中文名称">{pokemon.name}</Descriptions.Item>
+          <Descriptions.Item label="英文名称">{pokemon.name_en}</Descriptions.Item>
+          <Descriptions.Item label="宝可梦编号">#{String(pokemon.pokemon_id).padStart(3, '0')}</Descriptions.Item>
+          <Descriptions.Item label="属性类型">
+            <Space wrap>
+              {pokemon.types.map(({ type, slot }) => (
+                <TypeTag
+                  key={type.name}
+                  color={type.color}
+                  style={{ backgroundColor: type.color, color: 'white' }}
+                >
+                  {type.name} (槽位 {slot})
+                </TypeTag>
+              ))}
+            </Space>
+          </Descriptions.Item>
+        </Descriptions>
+      </Modal>
+    </>
   );
 };
 
